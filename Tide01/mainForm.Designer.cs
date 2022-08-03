c78c7e9be62f795169fb4351d38af4925765efa8
Mehrzad Khoddam <mehrzadkhdm@gmail.com>
2022-08-03 17:08:48 -0500

    filter for invalid data

--- Tide01/mainForm.Designer.cs
+++ Tide01/mainForm.Designer.cs
@@ -29,20 +29,37 @@ namespace Tide01
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(749, 38);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read port 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(916, 566);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "mainForm";
             this.Text = "Tide";
+            this.Load += new System.EventHandler(this.mainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Tide01/mainForm.cs
+++ Tide01/mainForm.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OceanTide;
+using PandasNet;
+using Tensorflow;
 
 namespace Tide01
 {
@@ -15,6 +19,65 @@ namespace Tide01
         public mainForm()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const double RAD = Math.PI / 180.0;
+            ForemanTide foremanTide = new ForemanTide(2022,1, 1, 0, 0, 0, 30.0);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(foremanTide.Constituents.Aggregate("Height", (s, x) => s + "," + $"C_{x.Key},S_{x.Key}"));
+
+            String b;
+            List<HourlyHeights> allValues = File.ReadAllLines( Application.StartupPath + "\\Data\\h126.csv")
+                                            .Select(v => HourlyHeights.FromCsv(v))
+                                            .ToList();
+
+            List<HourlyHeights> values = allValues.Where(v => v.year == 2016 && v.month==1 && v.height > 0).ToList();
+            //List<HourlyHeights> values = allValues.Where(v => v.height > 0).ToList();
+
+            double msl = values.Average(x => x.height);
+
+            foreach (HourlyHeights value in values)
+            {
+                foremanTide = new ForemanTide(value.year, value.month, value.day, value.hour, 0, 0, 30.0);
+                //var a = foremanTide.Constituents.Select(x => new {x.Value.name ,Cname = "C" + x.Key, Carg= Math.Cos(RAD * x.Value.argument),
+                //    Sarg = Math.Sin(RAD * x.Value.argument), x.Value.nodefactor });
+                //var b = foremanTide.Constituents.Aggregate("", (s, n) => s + ",C_" + n.Value.name);
+                b = foremanTide.Constituents.Aggregate($"{value.height-msl:F2}", (s, x) => s + "," + $"{Math.Cos(RAD * x.Value.argument)* x.Value.nodefactor :F7}, {Math.Sin(RAD * x.Value.argument) * x.Value.nodefactor:F7}");
+                sb.AppendLine(b);
+                //foremanTide.Constituents
+            }
+            File.WriteAllText(Application.StartupPath + "\\Data\\h126_2016_1m.csv", sb.ToString());
+            string aa = sb.ToString();
+            MessageBox.Show("Done");
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class HourlyHeights
+    {
+        public int year;
+        public int month;
+        public int day;
+        public int hour;
+        public int height;
+
+        public static HourlyHeights FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            HourlyHeights hourlyHeights = new HourlyHeights();
+
+            hourlyHeights.year = int.Parse(values[0]);
+            hourlyHeights.month = int.Parse(values[1]);
+            hourlyHeights.day = int.Parse(values[2]);
+            hourlyHeights.hour = int.Parse(values[3]);
+            hourlyHeights.height = int.Parse(values[4]);
+            
+            return hourlyHeights;
         }
     }
 }
